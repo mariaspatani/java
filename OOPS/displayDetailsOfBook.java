9344cb224026cc008d7ccf69b73dcdd33a2f9cfc
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-07-18 09:49:41 +0530

    Create displayDetailsOfBook.java

---
+++ OOPS/displayDetailsOfBook.java
@@ -0,0 +1,25 @@
+package Book;
+class Book{
+	String title;
+	String author;
+	Double price;
+	Book(){
+		title="Unknown";
+		author="Not assigned";
+		price=0.0;			
+	}
+	public void displayDetails() {
+		System.out.println("Title: \t"+title);
+		System.out.println("Author: \t"+author);
+		System.out.println("Price:\t"+price);
+	}
+}
+public class Demo {
+	public static void main(String[] args) {
+		Book book=new Book();
+		book.title="Unknown";
+		book.author="Not Assigned";
+		book.price="0";
+	}
+
+}
