33daeb1640dc20d93a0a362b86cc7a16fc401491
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-08-08 10:52:13 +0530

    Create InheritanceQn3.java

---
+++ OOPS/InheritanceQn3.java
@@ -0,0 +1,49 @@
+class Shape{
+	String color;
+	Shape(String color){
+		this.color=color;
+	}
+	public void displayColor() {
+		System.out.println("Color: "+color);
+	}
+}
+class Circle extends Shape{
+	double radius;
+	Circle(String color,double radius){
+	super(color);
+	this.radius=radius;	
+    }
+    public void calculateArea(double radius){
+    	double area=3.14*radius*radius;
+    	System.out.println("Area of Circle: "+area);   	
+    }
+}
+class Rectangle extends Shape{
+	double length;
+	double width;
+	Rectangle(String color,double length,double width){
+		super(color);
+		this.length=length;
+		this.width=width;
+	}
+	public void displayColor() {
+		System.out.println("Color: "+color);
+	}
+	public void displayColor() {
+		System.out.println("Color: "+color);
+	}
+	public void calculateArea(double length,double width) {
+		double area=length*width;
+		System.out.println("Area of Rectangle: "+area);		
+	}
+}
+public class Demo3 {
+	public static void main(String[] args) {
+		Circle circle=new Circle("White",2.5);
+		circle.displayColor();
+		circle.calculateArea(2.5);
+		Rectangle rectangle=new Rectangle("Red",5.0,5.0);
+		rectangle.displayColor();
+		rectangle.calculateArea(5.0, 5.0);
+	}
+}
