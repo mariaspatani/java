49c142b43c563e5f39e3f3d99a3e0691826e258c
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-09-04 17:49:04 +0530

    Demo.java

--- ExceptionHandling/Demo.java
+++ ExceptionHandling/Demo.java
@@ -1,1 +1,31 @@
-
+//Exception Handling 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class ExceptionHandlingDemo{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+	 try {
+		
+		System.out.println("Enter a whole number to divide: ");
+		int x=sc.nextInt();
+		
+		System.out.println("Enter a whole number to divide: ");
+		int y=sc.nextInt();
+		
+		int z=x/y;
+		System.out.println("result: "+z);
+	}
+	catch(ArithmeticException e){
+		System.out.println("you can't divide by zero!");
+	}
+	catch(InputMismatchException e){
+		System.out.println("please enter a number");
+	}
+	 catch(Exception e) {
+		 System.out.println("Something went wrong");
+	 }
+	 finally {
+		 sc.close();
+	 }
+  }
+} 
