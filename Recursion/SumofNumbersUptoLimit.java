8ebc2fabb04745a8d45d59fba5f869eaff9bc1df
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-09-05 01:12:06 +0530

    SumofNumbersUptoLimit.java

--- Recursion/SumofNumbersUptoLimit.java
+++ Recursion/SumofNumbersUptoLimit.java
@@ -1,1 +1,25 @@
+/*
+ Author : Maria
+ Date   : Sept 5 2025
+ Description : Find sum of the numbers upto a limit using recursion.
+ */
+import java.util.Scanner;
+public class SumofNosUptoLimit {
+	public static int sum (int n) {
+		if(n>0) {
+			return (n+sum(n-1));
+		}
+		else {
+			return 0;
+		}
+	}
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the limit");
+	int n=sc.nextInt();
+	int result=sum(n);
+	System.out.println("Sum: "+result);
+	sc.close();
+}
+}
 
