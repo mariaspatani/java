d2d971e81367e20d11c598ad36e84f8d60d78f90
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-09-08 22:28:19 +0530

    File_intro.java

---
+++ File/File_intro.java
@@ -0,0 +1,19 @@
+/*
+ Author : Maria
+ Date   : Sept 8 2025
+ Description : File introduction
+ */
+import java.io.File;
+public class FileDemo {
+	public static void main(String[] args){
+		//file =An abstract representation of file and directory pathnames
+	//creating an object for the file
+		File file=new File("FileDemo1.txt");
+		if(file.exists()) {
+			System.out.println("That file exists!");
+		}
+		else {
+			System.out.println("That doesn't file exists!");
+		}
+	}
+}
