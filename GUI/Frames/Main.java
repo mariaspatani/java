24cb236c5d221f185343d95b7f0492c45b21c52c
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-09-13 00:28:45 +0530

    Create Main.java

---
+++ GUI/Frames/Main.java
@@ -0,0 +1,21 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class gui_demo1 {
+	public static void main(String[] args) {
+		//JFrames= a GUI window to add components
+		 /*JFrame frame = new JFrame();//create a frame
+		 frame.setTitle("JFrame title goes here");
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out of the application
+		 frame.setResizable(false);//prevent frame from being resized
+		 frame.setSize(420,420);//set x and y dimension
+		 frame.setVisible(true);
+		 
+		 ImageIcon image = new ImageIcon("clear.png");//create imageicon
+		 frame.setIconImage(image.getImage());//change icon of the frame
+		 frame.getContentPane().setBackground(Color.BLUE);//change color of the background
+	*/
+		 new MyFrames();
+	}
+}
