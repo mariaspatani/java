d6c95cc42e8e26747e8386cc1ee2fad4196ae70b
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-09-22 01:22:08 +0530

    pat5.java

---
+++ Patterns/pat5.java
@@ -0,0 +1,31 @@
+/*
+Output :
+7
+7 6
+7 6 5
+7 6 5 4
+7 6 5 4 3
+7 6 5 4 3 2
+7 6 5 4 3 2 1
+*/
+import java.util.Scanner;
+public class NumberPatternPrograms 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many rows you want in this pattern?");
+        int rows = sc.nextInt();
+  System.out.println("Here is your pattern....!!!");
+ 
+        for (int i = rows; i >= 1; i--)
+        {
+            for (int j = rows; j >= i; j--)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
