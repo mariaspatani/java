aa9c34ce5f07c0862039213bd059c63369388397
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-10-15 20:50:19 +0530

    Update Calculator.java

--- GUI/Calculator.java
+++ GUI/Calculator.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Calculator extends JFrame implements ActionListener {
-
+    
     private JTextField textField;
     private double num1, num2, result;
     private char operator;
