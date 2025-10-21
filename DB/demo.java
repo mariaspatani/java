d53a71f600e19484dda4d1c4ee0073fd82ae15a2
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-10-21 23:23:32 +0530

    demo.java

--- DB/demo.java
+++ DB/demo.java
@@ -4,7 +4,7 @@ public class CreateDB {
 	static final String JDBC_DRIVER="com.mysql.jdbc.Driver";
 	static final String DB_URL ="jdbc:mysql://localhost:3306/";
 
-	
+
 	//database username and password
 	static final String USER ="demo";
 	static  String PWD ="abc@123#";
