b76178564609e475b807282778691c355ef136dc
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-11-19 22:07:06 +0530

    Create Book, EBook, and PrintedBook classes
    Implement base class Book and derived classes EBook and PrintedBook with displayDetails method.

---
+++ PYQ/Q13.java
@@ -0,0 +1,19 @@
+/*
+Create a base class Book with the following:
+Instance variables:
+title (String)
+author (String)
+price (double)
+Constructor to initialize the above values.
+Method displayDetails() to print the book’s details.
+Create a derived class EBook that extends Book:
+Additional instance variable: fileSizeMB (double).
+Constructor to initialize all values (use super for inherited fields).
+Override displayDetails() to include file size information.
+Create another derived class PrintedBook that extends Book:
+Additional instance variable: numberOfPages (int).
+Constructor to initialize all values (use super for inherited fields).
+Override displayDetails() to include number of pages information.
+In the main() method:
+Create one object each of EBook and PrintedBook.
+Call displayDetails() on both objects.*/
