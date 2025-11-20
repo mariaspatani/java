648e2674a5908b118c85bb2d6cbe60b73a6c407a
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-11-20 16:05:31 +0530

    Create FindDuplicate.java

---
+++ array/FindDuplicate.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Duplicates {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        System.out.println("Duplicate elements:");
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (a[i] == a[j])
+                    System.out.println(a[i]);
+    }
+}
